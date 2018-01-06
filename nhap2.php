47d2f295fe4f4c925af5e46def13279c8ea55d1b
Nguyễn Hoàng Dung <34667214+hoangdung4420@users.noreply.github.com>
2018-01-06 12:00:02 +0700

    Delete nhap2.php

--- nhap2.php
+++
@@ -1,9 +0,0 @@
-<?php
-echo "hello wednesday";
-echo "hj";
-echo "this is bcd branch?";
-echo "this is bcd branch?";
-echo "this is bcd branch?";
-echo "this is bcd branch?";
-echo "this is bcd branch?";
-
